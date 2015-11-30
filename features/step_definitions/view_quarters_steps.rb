e89398d74bb7d10cf801b43287c0180154c425b7
Dan Buckland <danbucklan@gmail.com>
2015-11-30 13:53:08 +0000

    Fix step definitions to wait for ProgressBar to disappear before checking list

--- features/step_definitions/view_quarters_steps.rb
+++ features/step_definitions/view_quarters_steps.rb
@@ -1,5 +1,9 @@
 Then(/^I should see the names of each list from the Trello board$/) do
+  wait_for_element_does_not_exist("android.widget.ProgressBar")
   list_displayed = query("ListView AppCompatTextView", :text)
   expected_list = ["Q1 2015", "Q2 2015", "Q3 2015", "Q4 2015"]
-  raise 'List displayed wrong items' unless list_displayed == expected_list
+  unless list_displayed == expected_list
+    puts list_displayed
+    raise 'List displayed wrong items'
+  end
 end
