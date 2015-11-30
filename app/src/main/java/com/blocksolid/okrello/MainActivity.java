1c81c84355b2ea7edb9dda33ad988c417bff2ad9
Dan Buckland <danbucklan@gmail.com>
2015-11-30 13:52:32 +0000

    Update app to make a getLists call to Trello API using retrofit

--- app/src/main/java/com/blocksolid/okrello/MainActivity.java
+++ app/src/main/java/com/blocksolid/okrello/MainActivity.java
@@ -2,18 +2,33 @@ package com.blocksolid.okrello;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.blocksolid.okrello.api.TrelloApi;
+import com.blocksolid.okrello.model.TrelloList;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.GsonConverterFactory;
+import retrofit.Response;
+import retrofit.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String BASE_URL = "https://api.trello.com/1/";
+
     public static ProgressBar progressBar;
     public static Button getListsBtn;
+    public static List<TrelloList> trelloLists;
     public static ListView listView;
 
     @Override
@@ -40,20 +55,47 @@ public class MainActivity extends AppCompatActivity {
         progressBar.setVisibility(View.VISIBLE);
 
         // TODO make retrofit call to capture Lists to an array
-        // TODO get list names to an array of strings
-        // Manually created array for the purpose of testing
+        // Retrofit stuff starts here
+        // Retrofit stuff here
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-        // Populate list view with strings from array
-        ArrayList<String> trelloListsArray = new ArrayList<>();
-        trelloListsArray.add("Q1 2015");
-        trelloListsArray.add("Q2 2015");
-        trelloListsArray.add("Q3 2015");
-        trelloListsArray.add("Q4 2015");
+        TrelloApi trelloApi = retrofit.create(TrelloApi.class);
 
-        listView.setAdapter(new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, trelloListsArray));
+        String key = "cf2308ac2c68ab9a54037478108439e4";
 
-        // Hide progress indicator when done
-        progressBar.setVisibility(View.INVISIBLE);
+        // Make the call
+        final Call<List<TrelloList>> call = trelloApi.getLists(key);
+        call.enqueue(new Callback<List<TrelloList>>() {
 
+
+            @Override
+            public void onResponse(Response<List<TrelloList>> response, Retrofit retrofit) {
+                trelloLists = response.body();
+
+                ArrayList<String> trelloListsArray = new ArrayList<>();
+
+                for (TrelloList listItem : trelloLists) {
+                    trelloListsArray.add(listItem.getName());
+                }
+
+                // Populate list view with strings from array
+                listView.setAdapter(new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, trelloListsArray));
+
+                // Hide progress indicator when done
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                // Log error here since request failed
+                Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_LONG).show();
+                Log.d("Retrofit", t.getMessage());
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+
+        });
     }
 }
