b85f1b93363f2ee15b9ca6068a1c9e5a484b8fd6
Dan Buckland <danbucklan@gmail.com>
2016-01-05 13:53:37 +0000

    Add WIP step definition for viewing Key Result scores

--- features/step_definitions/view_key_results_steps.rb
+++ features/step_definitions/view_key_results_steps.rb
@@ -12,5 +12,14 @@ Then(/^I should see the Key Results for the first Objective in (Q\d\s?\d*)$/) do
 end
 
 Then(/^I should see the scores of each Key Result for the first Objective in (Q\d\s?\d*)$/) do |quarter|
-  pending # express the regexp above with the code you wish you had
+  trello_scores = @trello.get_Key_Results_scores(@trello.get_first_card_id)
+  @base.wait_for_progress_to_complete
+  app_scores = @screen.key_results.get_scores
+  # Does not scroll the list to view everything so will fail if the Trello list
+  # has more cards that can fit on the screen.
+  unless app_scores == trello_scores
+    puts 'Saw this on Trello:  ' + trello_scores.to_s
+    puts 'Saw this in the app: ' + app_scores.to_s
+    raise 'Scores displayed incorrectly in app'
+  end
 end
