985f766bcc1162885033711d456ad0d24f9ca11b
Dan Buckland <danbucklan@gmail.com>
2016-04-03 14:56:07 +0100

    Add basic Sinatra mock backend

---
+++ features/support/trello_mock_backend/trello_backend.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+require 'json'
+
+class TrelloBackend < Sinatra::Base
+
+  set :bind, '0.0.0.0'
+
+  set :lists do
+    JSON.parse(File.read('boards/lists/lists.json'))
+  end
+
+  get '/boards/*/lists' do
+    key = params['key']
+    fields = params['fields']
+    content_type :json
+    TrelloBackend.lists.to_json
+  end
+
+  get '/' do
+    "Trello mock backend running"
+  end
+end
