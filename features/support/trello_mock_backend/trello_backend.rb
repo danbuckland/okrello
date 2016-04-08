e5cb3b6bdcf1fcd8037a15e58e5b4395d5516f87
Dan Buckland <danbucklan@gmail.com>
2016-04-08 23:51:29 +0100

    Move response files into responses directory

--- features/support/trello_mock_backend/trello_backend.rb
+++ features/support/trello_mock_backend/trello_backend.rb
@@ -6,12 +6,10 @@ class TrelloBackend < Sinatra::Base
   set :bind, '0.0.0.0'
 
   set :lists do
-    JSON.parse(File.read('boards/lists/lists.json'))
+    JSON.parse(File.read('responses/boards/lists/lists.json'))
   end
 
   get '/boards/*/lists' do
-    key = params['key']
-    fields = params['fields']
     content_type :json
     TrelloBackend.lists.to_json
   end
