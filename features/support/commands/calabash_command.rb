717343669dffb6152c51df3efcc2c3050edf5c3e
Dan Buckland <dan.buckland@chelsea-apps.com>
2017-05-12 17:57:11 +0100

    Add hive specific command to calabash command

--- features/support/commands/calabash_command.rb
+++ features/support/commands/calabash_command.rb
@@ -4,7 +4,7 @@ class CalabashCommand < BlockingCommand
 
   def initialize profile
 
-    @cmd = 'calabash-android run app/build/outputs/apk/app-debug.apk -c'
+    @cmd = 'bundle exec calabash-android resign $APK_PATH bundle exec calabash-android run $APK_PATH -f Res::Formatters::RubyCucumber2 -o \"$HIVE_RESULTS/out.res\" -f pretty'
     @cmd = @cmd + ' -p ' + profile unless profile.nil?
 
   end
